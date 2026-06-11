05e125470f840f28f573ca251f0e48b03c20313e
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2026-06-11 20:58:04 +0200

    add storybook

---
+++ src/components/list/list.tsx
@@ -0,0 +1,11 @@
+import { ListItem } from "./list-item";
+
+export const List = () => {
+  return (
+    <ul>
+      <ListItem>Item 1</ListItem>
+      <ListItem>Item 2</ListItem>
+      <ListItem>Item 3</ListItem>
+    </ul>
+  );
+};
